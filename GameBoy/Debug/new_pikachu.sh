ceef543cb75a8bd0f0867fb4ee56dff496eadfde
magali modugno <magalimodugno@gmail.com>
2020-07-24 11:29:24 -0300

    saco los gitignores de debug

---
+++ GameBoy/Debug/new_pikachu.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+LD_LIBRARY_PATH="/home/utnso/tp-2020-1c-The-X-Team/utils/Debug"  ./gameboy GAMECARD NEW_POKEMON Pikachu 2 5 10 9
---
+++ GameBoy/Debug/new_pokemons_varios.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+LD_LIBRARY_PATH="/home/utnso/tp-2020-1c-The-X-Team/utils/Debug"  ./gameboy GAMECARD NEW_POKEMON Pikachu 1 9 3 1
+LD_LIBRARY_PATH="/home/utnso/tp-2020-1c-The-X-Team/utils/Debug" ./gameboy GAMECARD NEW_POKEMON Charmander 4 1 101 2
+LD_LIBRARY_PATH="/home/utnso/tp-2020-1c-The-X-Team/utils/Debug" ./gameboy GAMECARD NEW_POKEMON Charmander 517 2046 15 3
+LD_LIBRARY_PATH="/home/utnso/tp-2020-1c-The-X-Team/utils/Debug" ./gameboy GAMECARD NEW_POKEMON Charmander 413 17 1 4
+LD_LIBRARY_PATH="/home/utnso/tp-2020-1c-The-X-Team/utils/Debug" ./gameboy GAMECARD NEW_POKEMON Charmander 723 97 29 5
+LD_LIBRARY_PATH="/home/utnso/tp-2020-1c-The-X-Team/utils/Debug" ./gameboy GAMECARD NEW_POKEMON Charmander 65 93 3001 6
+LD_LIBRARY_PATH="/home/utnso/tp-2020-1c-The-X-Team/utils/Debug" ./gameboy GAMECARD NEW_POKEMON Charmander 12 17 34 7
+LD_LIBRARY_PATH="/home/utnso/tp-2020-1c-The-X-Team/utils/Debug" ./gameboy GAMECARD NEW_POKEMON Charmander 129 547 11 8
